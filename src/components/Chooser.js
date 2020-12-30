128d476b36504c359966e5df48fb89757a5e39a3
chip <chiposaur@gmail.com>
2020-12-30 16:25:00 -0600

    tweaks.

--- src/components/Chooser.js
+++ src/components/Chooser.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react'
-import { useRecoilState } from "recoil"
+import React, { useState, useEffect } from 'react'
+import { useRecoilState, useRecoilValue } from "recoil"
 import { jsonobj, xmlobj } from "../recoil/atoms"
+import { chkJSON, chkXML } from "../recoil/selectors";
 
 import { useUploader } from 'react-files-hooks';
 
 const Chooser = () => {
+	const [areBoth, setAreBoth] = useState(false)
 	const [JSONfile, setJSONfile] = useRecoilState(jsonobj)
 	const [XMLfile, setXMLfile] = useRecoilState(xmlobj)
+
+	const isJSON = useRecoilValue(chkJSON)
+	const isXML = useRecoilValue(chkXML)
+
 
 	const { uploader, reset } = useUploader({
 		onSelectFile: incoming => {
@@ -38,6 +44,14 @@ const Chooser = () => {
 	useEffect(() => {
 		// console.log(XMLfile)
 	}, [XMLfile])
+
+	useEffect(() => {
+		setAreBoth(isJSON && isXML)
+	}, [isJSON, isXML])
+
+	useEffect(() => {
+		areBoth ? console.log(`conlog: dexie`,) : console.log(`conlog: NOPE`,) 
+	}, [areBoth])
 
 	return (
 		<div>
