05d30d5c36ffb5a63253a3b7cac2935b86f57af7
chip <chiposaur@gmail.com>
2020-12-30 13:36:40 -0600

    chakra seems simple enough

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import React from 'react';
 import Chooser from "./components/Chooser"
 import Show from "./components/Show"
 
+import { Box, Container, StackDivider, Tabs, TabList, TabPanels, Tab, TabPanel, VStack } from "@chakra-ui/react"
+
 function App() {
 	return (
-		<>
-			<Chooser />
-			<br />
-			<Show />
-		</>
+		<Container maxW="xl">
+			<VStack
+				divider={<StackDivider borderColor="gray.200" />}
+				spacing={4}
+				align="stretch"
+			>
+				<Show />
+				<Tabs variant="enclosed-colored">
+					<TabList>
+						<Tab>Chooser</Tab>
+						<Tab>Editor</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>
+							<Chooser />
+						</TabPanel>
+						<TabPanel>
+							<p>Editor</p>
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</VStack>
+		</Container>
 	)
 }
 
