c2f2b4fefe996ef86fbb2aa14283fed013c8022e
chip <chiposaur@gmail.com>
2020-12-30 15:35:10 -0600

    <Show> formatting

--- src/components/Show.js
+++ src/components/Show.js
@@ -3,8 +3,16 @@ import { useRecoilState, useRecoilValue } from "recoil"
 import { jsonobj, xmlobj } from "../recoil/atoms"
 import { chkJSON, chkXML } from "../recoil/selectors";
 
-import { Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/react"
-import { HStack, VStack } from "@chakra-ui/react"
+import {
+	Alert, AlertIcon, AlertTitle, AlertDescription,
+	Wrap, WrapItem,
+	HStack, VStack,
+	Grid, GridItem,
+	Divider,
+	Text,
+	Badge,
+	Container,
+} from "@chakra-ui/react"
 
 const Show = () => {
 	const [areBoth, setAreBoth] = useState(false)
@@ -19,27 +27,38 @@ const Show = () => {
 	}, [isJSON, isXML])
 
 	return (
-		<HStack>
+		<VStack>
+			<br />
 			{
 				areBoth
-					? <Alert status="success">
-						<AlertTitle>Ready</AlertTitle>
-						<AlertDescription>to Edit.</AlertDescription>
+					? <Alert status="success" flexDirection="column" alignItems="left" rounded="md">
+						<HStack><AlertIcon /><AlertTitle w="30%">Ready</AlertTitle></HStack>
+						<AlertDescription fontSize="sm">Editor Unlocked.</AlertDescription>
 					</Alert>
-					: <Alert status="error">
-						<AlertIcon />
-						<AlertTitle>Not Ready.</AlertTitle>
-						<AlertDescription>Configuration not completely loaded. Use the Chooser tab to load the config files.</AlertDescription>
+					: <Alert status="error" flexDirection="column" alignItems="left" rounded="md">
+						<HStack><AlertIcon /><AlertTitle w="30%">Not Ready.</AlertTitle></HStack>
+						<AlertDescription fontSize="sm">Configuration not loaded. Use the <b>Chooser</b> tab to load the config files.</AlertDescription>
 					</Alert>
 			}
-			{isXML
-				? "inputUserMappings.xml: Loaded"
-				: "inputUserMappings.xml: NOT Loaded"}
-			<p />
-			{isJSON
-				? "UserSettings.json: Loaded"
-				: "UserSettings.json: NOT Loaded"}
-		</HStack>
+			<Grid fontSize="sm" w="100%" templateColumns="repeat(2, 1fr)" gap={6}>
+				<HStack>
+					<Text>inputUserMappings.xml</Text>
+					{
+						isXML
+							? <Badge align="center" w="100px" colorScheme="green" variant="outline">Ready</Badge>
+							: <Badge align="center" w="100px" colorScheme="red" variant="solid">NOT Loaded</Badge>
+					}
+				</HStack>
+				<HStack>
+					<Text>UserSettings.json</Text>
+					{
+						isJSON
+							? <Badge align="center" w="100px" colorScheme="green" variant="outline">Ready</Badge>
+							: <Badge align="center" w="100px" colorScheme="red" variant="solid">NOT Loaded</Badge>
+					}
+				</HStack>
+			</Grid>
+		</VStack>
 	)
 }
 
