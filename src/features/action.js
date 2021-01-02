bfb1d4a98b468eff2fc167722ba0324a4a553aa0
chip <chiposaur@gmail.com>
2021-01-02 12:38:57 -0600

    more features stuff.

--- src/features/action.js
+++ src/features/action.js
@@ -4,7 +4,6 @@ const feature = {
 	instruction: "Before using this to change and replace your config, use the game to change any existing bind to the key you want to use for your action key.\nFor example, if you want to use 'Q' as your action key, use the game to change the 'Lean' action to something else, otherwise, you'll 'Lean' everytime you use the action key to open a door.\nAfter using this to change your action key, 'F' will be available to bind to other things.",
 	xml: {
 		seek: ["\"IK_F\""],
-		replace: ["\"IK_Q\""],
 	},
 	json: {
 		seek: [],
--- src/features/index.js
+++ src/features/index.js
@@ -1,1 +1,2 @@
 import "./action.js"
+const features = require("./")
